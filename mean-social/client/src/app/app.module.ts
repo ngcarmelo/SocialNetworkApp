ace114d06b7315acd8bd994f47678e908052807f
Carmelo Navarro <ng.carmelo@gmail.com>
2018-10-31 15:51:22 +0100

    adding following component

--- mean-social/client/src/app/app.module.ts
+++ mean-social/client/src/app/app.module.ts
@@ -27,6 +27,7 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { TimelineComponent } from './components/timeline/timeline.component';
 import { PublicationsComponent } from './components/publications/publications.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { FollowingComponent } from './components/following/following.component';
 
 //Aqui tambien indicamos los componentes, directivas, pipes:
 @NgModule({
@@ -40,7 +41,8 @@ import { ProfileComponent } from './components/profile/profile.component';
     SidebarComponent,
     TimelineComponent,
     PublicationsComponent,
-    ProfileComponent
+    ProfileComponent,
+    FollowingComponent
   ],
   imports: [     //cargar modulos
     BrowserModule,
